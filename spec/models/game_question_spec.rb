9177168d07f7f548f5d095a764006b0b83a0cb4e
LunST <lunststunt@gmail.com>
2021-05-28 00:42:39 +0300

    test correct_answer_key

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -53,4 +53,10 @@ RSpec.describe GameQuestion, type: :model do
       expect(game_question.level).to eq(game_question.question.level)
     end
   end
+
+  context 'correct_answer_key' do
+    it 'current answer' do
+      expect(game_question.correct_answer_key).to eq('b')
+    end
+  end
 end

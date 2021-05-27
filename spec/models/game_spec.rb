a75534ff5159d6a7174693dd1d454961b9f7708b
LunST <lunststunt@gmail.com>
2021-05-28 00:58:38 +0300

    spec current level / previous_level

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -108,4 +108,14 @@ RSpec.describe Game, type: :model do
       expect(game_w_questions.status).to eq(:money)
     end
   end
+
+  context 'current game question' do
+    it 'current level' do
+      expect(game_w_questions.current_game_question).to eq(game_w_questions.game_questions[0])
+    end
+
+    it 'previous_level ' do
+      expect(game_w_questions.previous_level).to eq(-1)
+    end
+  end
 end
